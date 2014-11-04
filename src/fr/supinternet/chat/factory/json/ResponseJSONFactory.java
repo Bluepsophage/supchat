4810a6750475b41cfde0748b3f9044f65d1ecb41
Benoit <benoit@hedonic-technologies.com>
2014-11-04 11:20:00 +0100

    Ajout de la factory Response

---
+++ src/fr/supinternet/chat/factory/json/ResponseJSONFactory.java
@@ -0,0 +1,46 @@
+package fr.supinternet.chat.factory.json;
+
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+import fr.supinternet.chat.model.Response;
+import fr.supinternet.chat.model.ResponseCode;
+import fr.supinternet.chat.model.User;
+
+public class ResponseJSONFactory {
+	
+private static final String TAG = "TokenJsonFactory";
+	
+	public static JSONObject getJSONObject(Response r) throws JSONException{
+		
+		if (r == null){
+			Log.e(TAG, "Unable to create JSONObject from Response caused by Response null");
+			return null;
+		}
+		
+		JSONObject result = new JSONObject();
+		result.accumulate("code", r.getCode());
+		result.accumulate("status", r.getStatus());
+		return result;
+	}
+	
+	public static User parseFromJSONObject(JSONObject json) throws JSONException{
+		
+		if (json == null){
+			Log.e(TAG, "Unable to create Response from Json caused by json null");
+			return null;
+		}
+		
+		Response result = new Response();
+		
+		result.setCode(ResponseCode.valueOf(json.getString("code")));
+		result.setStatus(json.getString("status"));
+		
+		return result;
+	}
+	
+}
