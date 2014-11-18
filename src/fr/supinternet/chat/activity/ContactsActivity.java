bf8b989a074155129aceab900dd9c0d68c6540a4
Benoit <benoit@hedonic-technologies.com>
2014-11-18 11:32:26 +0100

    Mise en place de la page CreateChat

--- src/fr/supinternet/chat/activity/ContactsActivity.java
+++ src/fr/supinternet/chat/activity/ContactsActivity.java
@@ -10,6 +10,7 @@ public class ContactsActivity extends Activity{
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_contacts);
+		
 	}
 
 }
